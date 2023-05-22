ee7e58a20f9cdb527c8110dc4d87570e2c332b0c
amanda hernandez sandate <amandahernandezsandate@amandas-air.attlocal.net>
2023-05-21 21:25:37 -0500

    add test video

--- lanes_video.py
+++ lanes_video.py
@@ -8,7 +8,7 @@ while(capture.isOpened()):
     _, img_frame = capture.read()
     canny_img = lanes_picture.gradient(img_frame)
     cropped_img = lanes_picture.region_of_interest(canny_img)
-    lines = cv2.HoughLinesP(cropped_img, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+    lines = cv2.HoughLinesP(cropped_img, 2, np.pi/180, 30, np.array([]), minLineLength=40, maxLineGap=5)
     avg_lines = lanes_picture.avg_slope_intercept(img_frame, lines)
     line_img = lanes_picture.display_lines(img_frame, avg_lines)
     fin_img = cv2.addWeighted(img_frame, 0.8, line_img, 1, 1)
